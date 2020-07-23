a7374e9b5c84d385e71cb3d436301b014d44f1c2
smreeti <mool.smreeti@gmail.com>
2020-07-23 16:47:32 +0545

    hello world

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "./Hello";
 
-ReactDOM.render(<h1> Hello World!!</h1>, document.getElementById("root"));
-
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<h1> Hello World</h1>, document.getElementById("root"));
